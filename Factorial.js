c137b5b1c950b96c205051ca519fcee798b16d7f
Kawsar-95 <virtualkawsar@gmail.com>
2020-07-16 14:29:25 +0600

    change

--- Factorial.js
+++ Factorial.js
@@ -1,9 +1,28 @@
-function factorial(n){
+//Using For Loop
+
+function fact(n){
 var factorial = 1;
 for(i=1; i<=10;i++){
     factorial=factorial*i;
     console.log(i,factorial);
 }
 return factorial;
-}var result = factorial(10);
+}var result = fact(10);
 console.log(result);
+
+
+//  Using While Loop
+
+function fact(n){
+var i =1;
+var factorial = 1;
+while(i<=n){
+    factorial=factorial*i;
+    // console.log(i,factorial);
+    i++;
+}
+return factorial;
+}
+var result = fact(10);
+console.log(result);
+
