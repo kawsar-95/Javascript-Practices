57dd5eae9fdabf269945857750dba874ca773570
kawsar-95 <virtualkawsar@gmil.com>
2020-07-19 04:27:46 -0700

    wwho is bigger

---
+++ max.js
@@ -0,0 +1,23 @@
+var business = 450;
+var minister = 650;
+var consulate =550;
+
+
+if(business> minister){
+    // console.log("business is bigger ");
+    if (business>consulate){
+        console.log("business is bigger");
+    }
+    else{
+        console.log ("consulate is bigger");
+    }
+}
+else{
+    // console.log("minister is bigger");
+    if (minister>consulate){
+        console.log("minister is bigger");
+    }
+    else{
+        console.log("consulate is bigger");
+    }
+}
