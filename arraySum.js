fe8ce3ed3966d28389aaff99b70e7edd635a4261
kawsar-95 <virtualkawsar@gmil.com>
2020-07-19 22:54:42 +0600

    done

--- arraySum.js
+++ arraySum.js
@@ -1,4 +1,4 @@
-    function getArraySum(number)
+    function getArraySum(numbers)
     {
         var sum = 0;
         for(i=0;i<numbers.length;i++){
@@ -7,6 +7,11 @@
         }
         return sum;
     }
-    var numbers=[11, 22, 33,44, 55,66,77,88,99];
+    var numbers=[11, 22, 33, 44, 55, 66, 77, 88, 99];
     var result = getArraySum(numbers);
     console.log("Total of the numbers:",result);
+
+    
+    var result = getArraySum([12,24,36,48,60,72,84,96,108]);
+    console.log("Total of the numbers:",result);
+
