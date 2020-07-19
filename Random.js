0ce71974d03017ee9635c14b70d65ea64e464df5
kawsar-95 <virtualkawsar@gmil.com>
2020-07-19 01:25:57 -0700

    random

---
+++ Random.js
@@ -0,0 +1,10 @@
+var num =2.4999999;
+var result =Math.floor(num);
+var result1 =Math.ceil(num);
+var result2 =Math.round(num);
+for(var i=0;i<10;i++){
+    var randonNum = Math.random()*10;
+    var output =Math.round(randonNum);
+    console.log(output);
+
+}
