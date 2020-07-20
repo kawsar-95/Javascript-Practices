a538b1e8d3b3864a2d0f1a28f36d182cb2cffab4
kawsar-95 <virtualkawsar@gmail.com>
2020-07-20 22:14:31 +0600

    done

--- jungleAnimal.js
+++ jungleAnimal.js
@@ -1,9 +1,23 @@
-var deapth = 35;
-var animal =0;
-if(deapth<=10){
-    animal = deapth*50;
-    else if(deapth<=20){
-        
+    function animalCalc(depth){
+    var animal =0;
+    if(depth<=10){
+        animal = depth*50;
+        }
+        else if(depth<=20){
+            var first10= 10*50;
+            var remaining = depth -10;
+            var second10 = remaining*100;
+            animal = first10+ second10;
     }
+    else{
+        var first10 = 10*50;
+        var second10 = 10*100;
+        var remaining= depth-20;
+        var third10 = remaining *300;
+        animal = first10+second10+third10;
+    }
+    return animal;
 
-}
+    }
+    var count = animalCalc(22);
+    console.log(count);
