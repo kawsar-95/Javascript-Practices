541f8f6ce3b5723dc2297516a97bca1e531af4c2
kawsar-95 <virtualkawsar@gmail.com>
2020-07-20 17:41:47 +0600

    done

---
+++ recursion_Bonus.js
@@ -0,0 +1,21 @@
+function fact(num){
+    var factorial = 1;
+    for(var i=1; i<=num;i++){
+        factorial=factorial*i;
+        console.log(i,factorial);
+    }
+    return factorial;
+    }
+    function recursiveFactorial(num){
+        if (num ==1){
+            console.log(num);
+            return 1;
+        }
+        else{ 
+            console.log(num,num-1);
+            return num*recursiveFactorial(num -1);
+        }
+
+    }
+    var result = recursiveFactorial(5);
+    console.log('result',result);
