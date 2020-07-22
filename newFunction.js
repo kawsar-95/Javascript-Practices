1d7eca53b489bcf8a0063072519d68b9ab6217de
kawsar-95 <virtualkawsar@gmail.com>
2020-07-22 12:49:20 +0600

    done

--- newFunction.js
+++ newFunction.js
@@ -1,19 +1,17 @@
-function evenifier(num){
-    if(num % 2== 0){
+function evenify_all(nums){
+    for (let i= 0; i<nums.length;i++) {
+        const num = nums[i];
+        
+        if(num % 2== 0){
         console.log(num, ':is even number');
-    }
-    else{
+        }
+        else{
         console.log(num*2,': is odd number');
+        }
     }
 }
-numb =[5,10,15,20,25,30];
-for (let i= 0; i<numb.length;i++) {
-    const num = numb[i];
-    evenifier(num);
-    
-}
+
+nums =[5,10,15,20,25,30];
+evenify_all(nums);
  friends_age =[24,23,25,26,22];
- for (let i = 0;i < friends_age.length;i++) {
-     const age = friends_age[i];
-     evenifier(age);
- }
+evenify_all(friends_age);
