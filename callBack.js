6a4315c41ebc87d991fa09d6fb6186a9c1fa661e
kawsar-95 <virtualkawsar@gmail.com>
2020-07-22 16:08:49 +0600

    done

---
+++ callBack.js
@@ -0,0 +1,19 @@
+function callBack(name,age,task){
+    console.log('Hello',name);
+    console.log('Your age',age);
+    task();
+}
+function washHand(){
+    console.log('wash your hand');
+}
+function takeShower(){
+    console.log('go and take shower');
+}
+function swapTinder(){
+    console.log('go and take a Date');
+}
+callBack('Jonas',24,washHand);
+callBack('martha',20,takeShower);
+callBack('claudia',30,swapTinder);
+
+
